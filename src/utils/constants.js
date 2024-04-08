e3aed24d557fd9eb9b9d23d3939503ea0f381c92
Joetech12 <ifeanyi4umeh@gmail.com>
2024-04-08 19:53:38 +0100

    adjusted .env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -32,21 +32,21 @@ import {
   faSquarePollVertical,
 } from "@fortawesome/free-solid-svg-icons";
 
-export const backendAPI = process.env.REACT_APP_API_URL;
+// export const backendAPI = process.env.REACT_APP_API_URL;
 
-// export const backendUrl = function () {
-//   if (window.location.href.includes("https://staging.schoolmateglobal.com")) {
-//     return process.env.REACT_APP_API_URL2; // Update with your development API URL
-//   } else if (
-//     window.location.href.includes("https://dashboard.schoolmateglobal.com")
-//   ) {
-//     return process.env.REACT_APP_API_URL2;
-//   } else {
-//     return process.env.REACT_APP_API_URL;
-//   }
-// };
+export const backendUrl = function () {
+  if (window.location.href.includes("https://staging.schoolmateglobal.com")) {
+    return process.env.REACT_APP_API_URL2; // Update with your development API URL
+  } else if (
+    window.location.href.includes("https://dashboard.schoolmateglobal.com")
+  ) {
+    return process.env.REACT_APP_API_URL2;
+  } else {
+    return process.env.REACT_APP_API_URL;
+  }
+};
 
-// export const backendAPI = backendUrl();
+export const backendAPI = backendUrl();
 
 export const homeUrl = {
   Superadmin: "/app/super-admin",
