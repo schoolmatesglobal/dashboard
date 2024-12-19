e71ae203bfd041ff91e997d569247d9176b5dcc7
Joetech12 <ifeanyi4umeh@gmail.com>
2024-12-19 19:24:23 +0100

    updated api url back to dashboard

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -38,9 +38,9 @@ import {
 // export const backendAPI = process.env.REACT_APP_API_URL;
 // const url1 = "https://goshenpillarsportal.schoolmateglobal.com/esc/api";
 const url1 = "https://staging.sapsms.com/api";
-// const url2 = "https://dashboard.sapsms.com/api";
+const url2 = "https://dashboard.sapsms.com/api";
 // const url2 = "https://staging.sapsms.com/api";
-const url2 = "https://goshenpillarsportal.schoolmateglobal.com/esc/api";
+// const url2 = "https://goshenpillarsportal.schoolmateglobal.com/esc/api";
 
 export const backendAPI = (function () {
   if (window.location.href.includes("https://staging.schoolmateglobal.com")) {
